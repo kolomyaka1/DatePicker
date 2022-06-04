9fb1654e926888fff25f821ed0062a3215b01c15
kolomyaka1 <66018338+kolomyaka1@users.noreply.github.com>
2022-06-04 11:50:29 +0300

    Исправление бага при клике на последние дни месяца

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ datePickerElemAll.forEach((datePickerElem) => {
     function formatDate(date) {
 
         let day = date.getDate();
-        let month = date.getMonth();
+        let month = date.getMonth() +1;
         let year = date.getFullYear();
         if (month < 10) {
             month = '0' + month;
@@ -210,10 +210,9 @@ datePickerElemAll.forEach((datePickerElem) => {
             }
 
             dayElem.addEventListener('click', function (e) {
-        debugger
 
                 e.target.classList.add('current');
-                selectedDate = new Date(year,(month+1),i);
+                selectedDate = new Date(year,(month),i);
                 selectedDay = i;
                 selectedMonth = month;
                 selectedYear = year;
